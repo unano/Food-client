31dc48b16659a87e81c6bc5671fd9fc598291247
unano <romanroll886@gmail.com>
2021-12-14 21:33:07 +0800

    dsd

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 export const getFoods = () => {
     return fetch(
-        'https://food-web-server.herokuapp.com/api/foods', {
+        '/api/foods', {
             headers: {
                 'Content-Type': 'application/json'
             }
@@ -11,7 +11,7 @@ export const getFoods = () => {
 
 export const getFoodKinds = () => {
     return fetch(
-        'https://food-web-server.herokuapp.com/api/foodKinds', {
+        '/api/foodKinds', {
             headers: {
                 'Content-Type': 'application/json'
             }
